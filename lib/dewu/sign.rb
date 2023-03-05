c24975de609d1391a7884bca4f65a5907d74d1df
luoding <luoding@jinanlongen.com>
2023-03-05 12:27:39 +0800

    rename query to signature_string in sign

--- lib/dewu/sign.rb
+++ lib/dewu/sign.rb
@@ -5,7 +5,7 @@ module Dewu
     extend self
 
     def generate(params)
-      query = params.sort.map { |key, value|
+      signature_string = params.sort.map { |key, value|
         if value.is_a?(Array)
           value = value.map { |v|
             v.is_a?(String) ? v : v.to_json
@@ -19,7 +19,7 @@ module Dewu
         "#{key}=#{CGI.escape(value.to_s)}" if value && !value.to_s.empty?
       }.compact.join("&")
 
-      Digest::MD5.hexdigest("#{query}#{Dewu.app_secret}").upcase
+      Digest::MD5.hexdigest("#{signature_string}#{Dewu.app_secret}").upcase
     end
 
     def generate!(params)
