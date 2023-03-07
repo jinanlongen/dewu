5a55a8ef2f9479e20586a990aa9d2b0bfb321325
luoding <luoding@jinanlongen.com>
2023-03-07 17:25:35 +0800

    add some bidding query api

--- lib/dewu/service.rb
+++ lib/dewu/service.rb
@@ -4,31 +4,38 @@ require "active_support/core_ext/hash/conversions"
 require "dewu/result"
 
 module Dewu
+  #https://open.dewu.com/#/index
   module Service
     extend self
 
     # 获取商家已授权的品牌和类目
-    # https://open.dewu.com/#/api/body?apiId=21&id=2&title=%E5%95%86%E5%93%81%E6%9C%8D%E5%8A%A1API
     def auth_brand_list(params = {})
       get("v1/spu/auth_brand_list", params)
     end
 
     # 根据品牌和类目ID拉取平台商品升级版【跨境】
-    # https://open.dewu.com/#/api/body?apiId=48&id=2&title=%E5%95%86%E5%93%81%E6%9C%8D%E5%8A%A1API
-    def brand_category_spus_oversea_sync(params = {})
-      get("v1/spu/oversea/brand_category_spus_oversea_sync", params)
+    def brand_category_spus_oversea_sync(brand_id, params = {})
+      get("v1/spu/oversea/brand_category_spus_oversea_sync", { brand_id: brand_id }.merge(params))
     end
 
     # 根据品牌和类目ID拉取平台商品升级版【国内】
-    # https://open.dewu.com/#/api/body?apiId=47&id=2&title=%E5%95%86%E5%93%81%E6%9C%8D%E5%8A%A1API
-    def brand_category_spus_domestic_sync(params = {})
-      get("v1/spu/domestic/brand_category_spus_domestic_sync", params)
+    def brand_category_spus_domestic_sync(brand_id, params = {})
+      get("v1/spu/domestic/brand_category_spus_domestic_sync", { brand_id: brand_id }.merge(params))
     end
 
     # 通过货号批量获取商品spu信息
-    # https://open.dewu.com/#/api/body?apiId=156&id=2&title=%E5%95%86%E5%93%81%E6%9C%8D%E5%8A%A1API
     def batch_article_number(article_numbers)
       post("v1/spu/batch_article_number", { article_numbers: article_numbers.split(",") })
+    end
+
+    # 商家出价列表信息（支持按出价类型查询）
+    def bidding_generic_list(bidding_type, params = {})
+      post("v1/bidding/generic_list", { bidding_type: bidding_type }.merge { params })
+    end
+
+    # 获取商家出价列表信息【普通现货】
+    def bidding_normal_list(bidding_type, sku_id, spu_id, merchant_sku_code, params = {})
+      post("v1/bidding/normal/normal_list", { sku_id: sku_id, spu_id: spu_id, merchant_sku_code: merchant_sku_code }.merge { params })
     end
 
     private
